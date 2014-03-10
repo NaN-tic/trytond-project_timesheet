f50c68688a604ab483cdb128ed8dd97814bb20ab
resteve <resteve@zikzakmedia.com>
2014-03-10 12:16:38 +0100

    Timesheet Line. Remove Order by Date (order in timesheet module)

--- __init__.py
+++ __init__.py
@@ -2,13 +2,3 @@
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 
-from trytond.pool import Pool
-from .line import *
-
-
-def register():
-    Pool.register(
-        Line,
-        module='project_timesheet', type_='model')
-
-
