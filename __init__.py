4e164f2afbd83d63deeda16482bdfde03f1fa7f9
Jordi Esteve <jesteve@zikzakmedia.com>
2014-08-02 20:01:49 +0200

    Add project_work to timesheet.line and fix work's timesheet lines relate. review http://codereview.tryton-erp.es/153/

--- __init__.py
+++ __init__.py
@@ -1,4 +1,12 @@
-#This file is part project_timesheet module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part project_timesheet module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.pool import Pool
+from .timesheet import *
 
+
+def register():
+    Pool.register(
+        TimesheetWork,
+        TimesheetLine,
+        module='project_timesheet', type_='model')
