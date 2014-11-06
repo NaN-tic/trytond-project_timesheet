4ef1315883c08d2d8352f6cc49927fc953544e96
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-06 10:55:30 +0100

    Cleanup tests

--- tests/test_project_timesheet.py
+++ tests/test_project_timesheet.py
@@ -1,34 +1,20 @@
 #!/usr/bin/env python
-#This file is part project_timesheet module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part project_timesheet module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT, test_view,\
-    test_depends
-from trytond.transaction import Transaction
+from trytond.tests.test_tryton import test_depends
 
 
 class ProjectTimesheetTestCase(unittest.TestCase):
-    '''
-    Test Project Timesheet module.
-    '''
+    'Test Project Timesheet module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('project_timesheet')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
@@ -37,6 +23,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         ProjectTimesheetTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
