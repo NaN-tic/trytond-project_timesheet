3c61fbe8d5fc6f341320a57101f3642e4b1e8672
resteve <resteve@zikzakmedia.com>
2015-12-01 11:16:53 +0100

    Tests

--- tests/test_project_timesheet.py
+++ tests/test_project_timesheet.py
@@ -1,21 +1,14 @@
-#!/usr/bin/env python
-# This file is part project_timesheet module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the project_timesheet module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ProjectTimesheetTestCase(unittest.TestCase):
+class ProjectTimesheetTestCase(ModuleTestCase):
     'Test Project Timesheet module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('project_timesheet')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'project_timesheet'
 
 
 def suite():
